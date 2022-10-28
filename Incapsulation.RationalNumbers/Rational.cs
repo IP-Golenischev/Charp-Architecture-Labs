7b86c19db8c2bfd432a4b97b38f8ee29938d34e6
Oleg Golenischev <oleg-golenischev63@yandex.ru>
2022-10-28 16:50:27 +0300

    all labs done

---
+++ Incapsulation.RationalNumbers/Rational.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Incapsulation.RationalNumbers
+{
+    public class Rational
+    {
+        public int Numerator { get; }
+        public int Denominator { get; }
+        public bool IsNan => Denominator == 0;
+        private static int gcd(int first, int second) => second == 0 ? first : gcd(second, first % second);
+
+        private static int lcd(int first, int second) => first * second / gcd(first, second);
+
+
+        public Rational(int numerator, int denominator)
+        {
+            if(denominator<0)
+            {
+                numerator = -numerator;
+                denominator = -denominator;
+
+            }
+            int nod = Math.Abs(denominator!= 0 ? gcd(numerator, denominator) : 1);
+            Numerator = numerator/nod;
+            Denominator = denominator/nod;
+
+        }
+        public Rational(int value) : this(value,1)
+        {
+
+        }
+        public static Rational operator *(Rational first, Rational second)
+            => new Rational(first.Numerator * second.Numerator, 
+                first.Denominator ==0 || second.Denominator ==0 ? 0 : first.Denominator * second.Denominator);
+        public static Rational operator /(Rational first, Rational second)
+    => new Rational(first.Numerator * second.Denominator,
+        first.Denominator == 0 || second.Denominator == 0 ? 0 : first.Denominator * second.Numerator);
+        public static Rational operator +(Rational first, Rational second)
+        {
+            int denominator = lcd(first.Denominator, second.Denominator);
+            if (denominator == 0) 
+                return new Rational(0, 0);
+            if (second.Denominator == 0 || first.Denominator == 0)
+                return new Rational(0, 0);
+            int numberator = first.Numerator * (denominator/first.Denominator) 
+                + second.Numerator*(denominator/second.Denominator);
+            return new Rational(numberator, denominator);
+        }
+        public static Rational operator -(Rational first, Rational second) => first + (second * new Rational(-1, 1));
+
+        public static implicit operator double(Rational r) => r.IsNan ? double.NaN : (double)r.Numerator / r.Denominator;
+
+        public static implicit operator Rational(int value)  => new Rational(value);
+
+        public static explicit operator int(Rational rational)
+        {
+            if (rational.IsNan || rational.Numerator % rational.Denominator != 0)
+            {
+                throw new ArgumentException();
+            }
+            return rational.Numerator / rational.Denominator;
+        }
+    }
+    
+}
---
+++ Incapsulation.Weights/Indexer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incapsulation.Weights
+{
+	class Indexer
+	{
+		private double[] _weights;
+		private  void ValidateIndex(int index)
+		{
+			if (index < 0 || index >= Length)
+				throw new IndexOutOfRangeException("Index must be more than zero and less than array length");
+		}
+		public  double this[int index]
+		{
+			get
+			{
+				ValidateIndex(index);
+				return _weights[_start+index];
+			}
+			set
+			{
+				ValidateIndex(index);
+				_weights[_start+ index] = value;
+			}
+		}
+		public int Length { get; }
+		private int _start;
+	
+		public Indexer(double[] weights, int start, int length)
+		{
+			if (start < 0 || start > weights.Length
+				|| length > weights.Length || start+length>weights.Length || length<0)
+				throw new ArgumentException("start and length must be less than array length");
+			_weights = weights;
+			Length = weights.Length;
+			_start = start;
+			Length = length;
+
+		}
+	}
+	
+}
